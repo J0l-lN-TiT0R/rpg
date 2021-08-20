64caac6cea5abec134729cd4aad6d164d49f1706
John Titor <J0l-lN_TiT0R@pm.me>
2021-08-20 11:34:30 +0300

    load player images; add scaling functionality to the SpriteSheet class

--- helper.py
+++ helper.py
@@ -6,9 +6,16 @@ import pygame as pg
 
 class SpriteSheet:
     """Class for storing and managing sprite sheets."""
-    def __init__(self, file_path):
-        """Load the sprite sheet from the given path."""
-        self.sheet = pg.image.load(file_path).convert_alpha()
+    def __init__(self, file_path, scale=1):
+        """Load the sprite sheet from the given path.
+
+        Scale the sheet by the scale parameter.
+        """
+        sheet = pg.image.load(file_path).convert_alpha()
+        w, h = sheet.get_size()
+        target_size = (int(w * scale), int(h * scale))
+        self.sheet = pg.transform.scale(sheet, target_size)
+        self.w, self.h = self.sheet.get_size()
 
     def get_image(self, x, y, width, height):
         """Cut and return a piece of sprite sheet."""
--- player.py
+++ player.py
@@ -13,8 +13,9 @@ class Player(pg.sprite.Sprite):
         """Initialize required variables."""
         super().__init__()
 
-        self.sprite_sheet = SpriteSheet(sprite_sheet_path)
-        self.image = self.sprite_sheet.get_image(0, 0, 32, 32)
+        sprite_sheet = SpriteSheet(sprite_sheet_path, 2)
+        self._load_images(sprite_sheet)
+        self.image = self.walk_right[0]
         self.rect = self.image.get_rect()
         self.rect.center = pos
 
@@ -44,3 +45,34 @@ class Player(pg.sprite.Sprite):
 
         self.velocity *= Player.speed
         self.rect.center += self.velocity
+
+    def _load_images(self, sheet):
+        self.walk_right = []
+        self.walk_left = []
+        self.walk_up = []
+        self.walk_down = []
+
+        # Version 1.0
+#         x = 0
+#         for _ in range(4):
+#             self.walk_down.append(sheet.get_image(x, 0, 32, 32))
+#             self.walk_left.append(sheet.get_image(x, 32, 32, 32))
+#             self.walk_right.append(sheet.get_image(x, 64, 32, 32))
+#             self.walk_up.append(sheet.get_image(x, 96, 32, 32))
+#             x += 32
+
+        # Version 2.0
+#         w, h = 32, 32
+#         for x in range(0, 128, 32):
+#             self.walk_down.append(sheet.get_image(x, 0, w, h))
+#             self.walk_left.append(sheet.get_image(x, 32, w, h))
+#             self.walk_right.append(sheet.get_image(x, 64, w, h))
+#             self.walk_up.append(sheet.get_image(x, 96, w, h))
+
+        # Version 3.0
+        w, h = sheet.w // 4, sheet.h // 4
+        for x in range(0, w*4, w):
+            self.walk_down.append(sheet.get_image(x, 0, w, h))
+            self.walk_left.append(sheet.get_image(x, h, w, h))
+            self.walk_right.append(sheet.get_image(x, h*2, w, h))
+            self.walk_up.append(sheet.get_image(x, h*3, w, h))
