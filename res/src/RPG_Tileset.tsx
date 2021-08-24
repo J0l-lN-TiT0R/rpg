1f5f35068203cedb99e64c12eaa74b28de635b42
John Titor <J0l-lN_TiT0R@pm.me>
2021-08-24 18:47:31 +0300

    add map files

---
+++ res/src/RPG_Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="RPG" tilewidth="16" tileheight="16" spacing="1" tilecount="136" columns="17">
+ <image source="kenney_monochromerpg/Default/Tilemap/tilemap.png" width="288" height="135"/>
+</tileset>
